6c202915fb2eef19d04b9261562c6bc1a1e89a78
Mert Erman | UNITED UP <mert.erman@forwardmc.com>
2026-07-08 22:06:33 +0300

    Update published site

--- _next/static/4CArk8Ne2vixbzit3_0Jw/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fhizmetler\u002F[slug]","\u002Fnasil-calisir\u002F[slug]","\u002Fneden-hadiyap\u002F[slug]","\u002Fozellikler\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
